c8b9dff403c4349a41d30d0bb2bc809dfdfb30b2
Pyhajarvi Maaret <maaret.pyhajarvi@vaisala.com>
2023-02-08 21:22:41 +0200

    Here you go

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ import requests
 local_env = "http://localhost:3002"
 hosted_env = "http://automationintesting.online"
 
+
 @pytest.fixture(scope= "function")
 def branding_resp() -> requests.Response:
     local = True
@@ -11,9 +12,21 @@ def branding_resp() -> requests.Response:
         r = requests.get(local_env + "/branding/")
     else:
         r = requests.get(hosted_env + "/branding/")
-    yield r
+    yield r, local
     print ("This is where we write teardown - things to do after test")
 
+@pytest.fixture(scope="function")
+def is_env_up(branding_resp):
+    if not branding_resp[1]:
+        raise Exception("Environment is not up")
+    else:
+        yield branding_resp[0]
+'''
+    with pytest.raises(Exception):
+        if not branding_resp[0].status_code == 200:
+            raise Exception("Environment is not up")
+            '''
+            
 @pytest.fixture(scope= "session")
 def admin() -> requests.Session:
     session = requests.Session()
@@ -21,3 +34,20 @@ def admin() -> requests.Session:
     {"username": "admin", "password": "password"})
     print(session)
     yield session
+
+
+from approvaltests import set_default_reporter
+from approvaltests.reporters import DiffReporter, quiet_reporter
+
+@pytest.fixture(scope="session", autouse=True)
+def reporter():
+    #set_default_reporter(DiffReporter())
+    set_default_reporter(quiet_reporter)
+
+
+
+'''
+def if_something():
+    """As a string the check is available for pytest before test run"""
+    return ‘”automationintesting.online in config.getoption("base_url")'
+    '''
--- test_1.py
+++ test_1.py
@@ -1,18 +1,18 @@
 from approvaltests import verify, verify_as_json
 import requests
 
-def test_branding(branding_resp):
-    assert branding_resp.status_code == 200
+def test_branding(is_env_up):
+    assert is_env_up.status_code == 200
     #Verify the entire json with approvaltests
-    verify_as_json(branding_resp.json())
+    verify_as_json(is_env_up.json())
     #Search for some string in the response text
-    assert "Shady Meadows" in branding_resp.text
+    assert "Shady Meadows" in is_env_up.text
     #Search for specific value in specific place of json-structure
-    assert branding_resp.json()["name"] == "Shady Meadows B&B"
+    assert is_env_up.json()["name"] == "Shady Meadows B&B"
     #Search deeper in structure and multiple items
-    assert branding_resp.json()["map"]["latitude"] == 52.6351204 and branding_resp.json()["map"]["longitude"] == 1.2733774
+    assert is_env_up.json()["map"]["latitude"] == 52.6351204 and is_env_up.json()["map"]["longitude"] == 1.2733774
     #Verify that there is a json structure present without caring about what is in it
-    assert branding_resp.json()["description"]
+    assert is_env_up.json()["description"]
 
 def test_change_branding(admin):
     response = admin.put("http://localhost/branding/", json = "")
