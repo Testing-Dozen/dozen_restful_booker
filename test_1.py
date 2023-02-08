62701ff93a2bd958bf2f90d384dc35f223b872c0
SvetlanaS1212 <svetlanas1212@gmail.com>
2023-02-08 19:56:44 +0200

    session 11 contents

--- test_1.py
+++ test_1.py
@@ -1,10 +1,11 @@
 from approvaltests import verify, verify_as_json
+import requests
 
 def test_branding(branding_resp):
     assert branding_resp.status_code == 200
     #Verify the entire json with approvaltests
     verify_as_json(branding_resp.json())
-    #Search for some strong in the response text
+    #Search for some string in the response text
     assert "Shady Meadows" in branding_resp.text
     #Search for specific value in specific place of json-structure
     assert branding_resp.json()["name"] == "Shady Meadows B&B"
@@ -13,10 +14,17 @@ def test_branding(branding_resp):
     #Verify that there is a json structure present without caring about what is in it
     assert branding_resp.json()["description"]
 
+def test_change_branding(admin):
+    response = admin.put("http://localhost/branding/", json = "")
+    assert response.status_code == 400
 
+def test_change_branding_without_admin():
+    response = requests.put("http://localhost/branding/", json = "")
+    assert response.status_code == 400
 
-def test_create_room(admin):
-    response = admin.post("http://localhost/room/", json={
+import pytest
+
+json1 = {
             "roomName": "999",
             "type": "Suite",
             "accessible": False,
@@ -25,9 +33,49 @@ def test_create_room(admin):
             "roomPrice": "878",
             "features": [
                 "WiFi",
-                "TV"]})
+                "TV"]}
+
+json2 = {
+            "roomName": "999",
+            "type": "Suite",
+            "accessible": True,
+            "description": "This is a lovely room",
+            "image": "https://www.mwtestconsultancy.co.uk/img/room1.jpg",
+            "roomPrice": "878",
+            "features": [
+                "WiFi",
+                "TV"]}
+
+@pytest.mark.parametrize("variable", [json1, json2])
+def test_create_room(admin, variable):
+    response = admin.post("http://localhost/room/", json = variable)
     assert response.status_code == 201
-    verify_as_json(response.text)
+    assert response.json()["roomName"] == "999"
+    assert response.json()["description"] == "This is a lovely room"
+    # verify_as_json(response.text)
+
+def test_post_validation(admin):
+    respone = admin.post("http://localhost/room/", json = "")
+    assert respone.status_code == 400
+
+def test_put_validation(admin):
+    respone = admin.put("http://localhost/room/1", json = "")
+    assert respone.status_code == 400
+
+json3 = {
+            "roomName": "777",
+            "type": "Suite",
+            "accessible": True,
+            "description": "This is a lovely room",
+            "image": "https://www.mwtestconsultancy.co.uk/img/room1.jpg",
+            "roomPrice": "878",
+            "features": [
+                "WiFi",
+                "TV"]}
+
+def test_put(admin):
+    respone = admin.put("http://localhost/room/1", json = json3)
+    assert respone.ok
 
 def test_get_rooms(admin):
     rooms = admin.get("http://localhost/room")
