bfc14a6d522b608cfa11d87a27205dae96987525
miipyh <62802307+miipyh@users.noreply.github.com>
2023-02-15 21:02:53 +0200

    some lint

--- test_message.py
+++ test_message.py
@@ -47,64 +47,65 @@ error_message_cleaned_to_dict_for_readability = {
         "email": "[NotBlank.message.email,NotBlank.email,NotBlank.java.lang.String,NotBlank], default message [email]]; default message [must not be blank]"}
 }
 
-full_error_message = {'error': 'BAD_REQUEST',
-                      'errorCode': 400,
-                      'errorMessage': 'Validation failed for argument [0] in public '
-                                      'org.springframework.http.ResponseEntity<com.automationintesting.model.db.Message> '
-                                      'com.automationintesting.api.MessageController.createMessage(com.automationintesting.model.db.Message) '
-                                      'throws java.sql.SQLException with 7 errors: [Field error in '
-                                      "object 'message' on field 'phone': rejected value []; codes "
-                                      '[NotBlank.message.phone,NotBlank.phone,NotBlank.java.lang.String,NotBlank]; '
-                                      'arguments '
-                                      '[org.springframework.context.support.DefaultMessageSourceResolvable: '
-                                      'codes [message.phone,phone]; arguments []; default message '
-                                      '[phone]]; default message [must not be blank]] [Field error '
-                                      "in object 'message' on field 'name': rejected value []; "
-                                      'codes '
-                                      '[NotBlank.message.name,NotBlank.name,NotBlank.java.lang.String,NotBlank]; '
-                                      'arguments '
-                                      '[org.springframework.context.support.DefaultMessageSourceResolvable: '
-                                      'codes [message.name,name]; arguments []; default message '
-                                      '[name]]; default message [must not be blank]] [Field error '
-                                      "in object 'message' on field 'description': rejected value "
-                                      '[hi, how are you]; codes '
-                                      '[Size.message.description,Size.description,Size.java.lang.String,Size]; '
-                                      'arguments '
-                                      '[org.springframework.context.support.DefaultMessageSourceResolvable: '
-                                      'codes [message.description,description]; arguments []; '
-                                      'default message [description],2000,20]; default message '
-                                      '[size must be between 20 and 2000]] [Field error in object '
-                                      "'message' on field 'subject': rejected value []; codes "
-                                      '[Size.message.subject,Size.subject,Size.java.lang.String,Size]; '
-                                      'arguments '
-                                      '[org.springframework.context.support.DefaultMessageSourceResolvable: '
-                                      'codes [message.subject,subject]; arguments []; default '
-                                      'message [subject],100,5]; default message [size must be '
-                                      "between 5 and 100]] [Field error in object 'message' on "
-                                      "field 'subject': rejected value []; codes "
-                                      '[NotBlank.message.subject,NotBlank.subject,NotBlank.java.lang.String,NotBlank]; '
-                                      'arguments '
-                                      '[org.springframework.context.support.DefaultMessageSourceResolvable: '
-                                      'codes [message.subject,subject]; arguments []; default '
-                                      'message [subject]]; default message [must not be blank]] '
-                                      "[Field error in object 'message' on field 'phone': rejected "
-                                      'value []; codes '
-                                      '[Size.message.phone,Size.phone,Size.java.lang.String,Size]; '
-                                      'arguments '
-                                      '[org.springframework.context.support.DefaultMessageSourceResolvable: '
-                                      'codes [message.phone,phone]; arguments []; default message '
-                                      '[phone],21,11]; default message [size must be between 11 and '
-                                      "21]] [Field error in object 'message' on field 'email': "
-                                      'rejected value []; codes '
-                                      '[NotBlank.message.email,NotBlank.email,NotBlank.java.lang.String,NotBlank]; '
-                                      'arguments '
-                                      '[org.springframework.context.support.DefaultMessageSourceResolvable: '
-                                      'codes [message.email,email]; arguments []; default message '
-                                      '[email]]; default message [must not be blank]] ',
-                      'fieldErrors': ['must not be blank',
-                                      'must not be blank',
-                                      'size must be between 20 and 2000',
-                                      'size must be between 5 and 100',
-                                      'must not be blank',
-                                      'size must be between 11 and 21',
-                                      'must not be blank']}
+full_error = {
+    'error': 'BAD_REQUEST',
+    'errorCode': 400,
+    'errorMessage': 'Validation failed for argument [0] in public '
+                    'org.springframework.http.ResponseEntity<com.automationintesting.model.db.Message> '
+                    'com.automationintesting.api.MessageController.createMessage(com.automationintesting.model.db.Message) '
+                    'throws java.sql.SQLException with 7 errors: [Field error in '
+                    "object 'message' on field 'phone': rejected value []; codes "
+                    '[NotBlank.message.phone,NotBlank.phone,NotBlank.java.lang.String,NotBlank]; '
+                    'arguments '
+                    '[org.springframework.context.support.DefaultMessageSourceResolvable: '
+                    'codes [message.phone,phone]; arguments []; default message '
+                    '[phone]]; default message [must not be blank]] [Field error '
+                    "in object 'message' on field 'name': rejected value []; "
+                    'codes '
+                    '[NotBlank.message.name,NotBlank.name,NotBlank.java.lang.String,NotBlank]; '
+                    'arguments '
+                    '[org.springframework.context.support.DefaultMessageSourceResolvable: '
+                    'codes [message.name,name]; arguments []; default message '
+                    '[name]]; default message [must not be blank]] [Field error '
+                    "in object 'message' on field 'description': rejected value "
+                    '[hi, how are you]; codes '
+                    '[Size.message.description,Size.description,Size.java.lang.String,Size]; '
+                    'arguments '
+                    '[org.springframework.context.support.DefaultMessageSourceResolvable: '
+                    'codes [message.description,description]; arguments []; '
+                    'default message [description],2000,20]; default message '
+                    '[size must be between 20 and 2000]] [Field error in object '
+                    "'message' on field 'subject': rejected value []; codes "
+                    '[Size.message.subject,Size.subject,Size.java.lang.String,Size]; '
+                    'arguments '
+                    '[org.springframework.context.support.DefaultMessageSourceResolvable: '
+                    'codes [message.subject,subject]; arguments []; default '
+                    'message [subject],100,5]; default message [size must be '
+                    "between 5 and 100]] [Field error in object 'message' on "
+                    "field 'subject': rejected value []; codes "
+                    '[NotBlank.message.subject,NotBlank.subject,NotBlank.java.lang.String,NotBlank]; '
+                    'arguments '
+                    '[org.springframework.context.support.DefaultMessageSourceResolvable: '
+                    'codes [message.subject,subject]; arguments []; default '
+                    'message [subject]]; default message [must not be blank]] '
+                    "[Field error in object 'message' on field 'phone': rejected "
+                    'value []; codes '
+                    '[Size.message.phone,Size.phone,Size.java.lang.String,Size]; '
+                    'arguments '
+                    '[org.springframework.context.support.DefaultMessageSourceResolvable: '
+                    'codes [message.phone,phone]; arguments []; default message '
+                    '[phone],21,11]; default message [size must be between 11 and '
+                    "21]] [Field error in object 'message' on field 'email': "
+                    'rejected value []; codes '
+                    '[NotBlank.message.email,NotBlank.email,NotBlank.java.lang.String,NotBlank]; '
+                    'arguments '
+                    '[org.springframework.context.support.DefaultMessageSourceResolvable: '
+                    'codes [message.email,email]; arguments []; default message '
+                    '[email]]; default message [must not be blank]] ',
+    'fieldErrors': ['must not be blank',
+                    'must not be blank',
+                    'size must be between 20 and 2000',
+                    'size must be between 5 and 100',
+                    'must not be blank',
+                    'size must be between 11 and 21',
+                    'must not be blank']}
